576baafce7be79c27684a3bc917325024243c46c
Emmanuel <sergio.are.ramos@gmail.com>
2023-01-09 19:00:43 -0600

    Primer commit

---
+++ Js.js
@@ -0,0 +1,39 @@
+
+function encriptar(){
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+    var textoCifrado = texto.replace(/e/igm,"enter");
+    var textoCifrado = textoCifrado.replace(/o/igm,"ober");
+    var textoCifrado = textoCifrado.replace(/i/igm,"imes");
+    var textoCifrado = textoCifrado.replace(/a/igm,"ai");
+    var textoCifrado = textoCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imageM").style.display = "none";
+    document.getElementById("text1").style.display = "none";
+    document.getElementById("text2").innerHTML = textoCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit"; 
+}
+
+function desencriptar(){
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+
+    var textoCifrado = texto.replace(/enter/igm,"e");
+    var textoCifrado = textoCifrado.replace(/ober/igm,"o");
+    var textoCifrado = textoCifrado.replace(/imes/igm,"i");
+    var textoCifrado = textoCifrado.replace(/ai/igm,"a");
+    var textoCifrado = textoCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imageM").style.display = "none";
+    document.getElementById("text1").style.display = "none";
+    document.getElementById("text2").innerHTML = textoCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit"; 
+}
+
+function copiar(){
+    var contenido = document.querySelector("#text2");
+    contenido.select();
+    document.execCommand("copy");
+    alert("Texto Copiado");
+}
